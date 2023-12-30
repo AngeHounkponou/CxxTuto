06d18b5fb4ca48c3317c8af3f57be2c41b1b9347
Ange Hounkponou <loic237@gmail.com>
2023-12-30 23:09:40 +0000

    Fonction spleat

--- read_csv.cxx
+++ read_csv.cxx
@@ -1,12 +1,43 @@
+#include <fstream>
 #include <iostream>
-#include_next <vector>
-#include_next <string>
-#include_next <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(){
+void f(string s_line, string separator = ",") {
+  size_t s_found = 0;
+  string s_value;
+  vector<string> v_line{};
+ 
+  while ((s_found = s_line.find(separator)) != string::npos) {
+    s_value = s_line.substr(0, s_found);
+    v_line.push_back(s_value);
+    s_line.erase(0, s_found + separator.length());
+  }
+  v_line.push_back(s_line);
 
-    cout<<"Hello"<<endl;
-    return 0;
+  for (auto i : v_line) {
+    cout << i << endl;
+  }
+}
+
+void read_csv(string fileName = "my_file.csv", string separator = ",") {
+  ifstream file;
+  string line{};
+
+  file.open(fileName);
+  if (file.is_open()) {
+    getline(file, line);
+
+    cout << line << endl;
+
+    file.close();
+  }
+}
+
+int main() {
+
+  f("For your giving of the blessing is certainly well done", " ");
+  return 0;
 }
